36b8b9d828004a75238688b0c6f26043d75931bf
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-03-19 14:35:00 -0300

    .

--- painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/controllers/VendedorController.java
+++ painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/controllers/VendedorController.java
@@ -1,10 +1,20 @@
 package com.api.painelvendas.controllers;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.painelvendas.models.VendedorModel;
 import com.api.painelvendas.services.VendedorService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -17,6 +27,8 @@ public class VendedorController {
 		this.vendedorService = vendedorService;
 	}
 	
-	
-	
+	@PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid VendedorModel vendedorModel){
+        return ResponseEntity.status(HttpStatus.CREATED).body(VendedorService.save(vendedorModel));
+		}
 }
--- painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/services/VendedorService.java
+++ painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/services/VendedorService.java
@@ -3,7 +3,11 @@ package com.api.painelvendas.services;
 
 import org.springframework.stereotype.Service;
 
+import com.api.painelvendas.models.VendedorModel;
 import com.api.painelvendas.repositories.VendedorRepository;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 
 @Service
 public class VendedorService {
@@ -12,5 +16,9 @@ public class VendedorService {
 	
 	public VendedorService(VendedorRepository vendedorRepository) {
 		this.vendedorRepository = vendedorRepository;
+	}
+	@Transactional
+	public static VendedorModel save(VendedorModel vendedorModel) {
+		return vendedorRepository.save(vendedorModel);
 	}
 }
