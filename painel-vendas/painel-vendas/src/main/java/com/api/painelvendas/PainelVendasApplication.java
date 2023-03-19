47e42429e9ef422c39df8c4715ede5d77b07f5c8
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-03-18 21:58:47 -0300

    Projeto inciado
    Projeto Spring boot gerado

---
+++ painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/PainelVendasApplication.java
@@ -0,0 +1,13 @@
+package com.api.painelvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelVendasApplication.class, args);
+	}
+
+}
