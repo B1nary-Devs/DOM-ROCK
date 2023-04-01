6e8542fd05f80c8f79038783c30c81a20fff8580
Marcelo Silva <101959064+marcelosilva7@users.noreply.github.com>
2023-03-31 22:48:33 -0300

    fix merge

--- painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.painelvendas.repositories;
-
-import com.api.painelvendas.models.ProdutoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<ProdutoModel, Integer> {
-}
