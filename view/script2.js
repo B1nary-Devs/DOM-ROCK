27dee823ec2bec660832e4d5fda70090ed1bdcf4
Davi Gusmão <davigusmao10@bol.com.br>
2023-04-16 00:15:50 -0300

    Alterando as telas (restando poucas modificações)

--- view/script2.js
+++ view/script2.js
@@ -42,3 +42,23 @@ async function buscarPlanejamento() {
   document.addEventListener('DOMContentLoaded', () => {
     buscarPlanejamento()
   });
+
+  $(function(){
+    $("#tabela input").keyup(function(){       
+        var index = $(this).parent().index(); /*index recebe como valor, a coluna que contém o input*/
+        var nth = "#tabela td:nth-child("+(index+1).toString()+")";
+        var valor = $(this).val().toUpperCase(); /*convertendo o texto para maiúsculo*/
+        $("#tabela tbody tr").show();
+        $(nth).each(function(){
+            if($(this).text().toUpperCase().indexOf(valor) < 0){ 
+                $(this).parent().hide(); /*retorna -1 se o valor informado não existir*/
+            }
+        });
+    });
+    
+    $("#tabela input").blur(function(){
+        $(this).val("");
+    }); 
+});
+
+
