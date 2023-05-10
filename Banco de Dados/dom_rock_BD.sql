873fa98035b7a9439f13da5f84e088fb444be618
CarlosTorres2305 <carlosadm15@hotmail.com>
2023-05-10 20:46:41 -0300

    alteração na predição

--- Banco de Dados/dom_rock_BD.sql
+++ Banco de Dados/dom_rock_BD.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE dom_rock;
+drop database dom_rock;
+use dom_rock;
 
 CREATE TABLE vendedor(
 id INT AUTO_INCREMENT PRIMARY KEY,
@@ -52,11 +54,11 @@ insert into produto(id, nome, tipo) values (3, 'barra de aço', 'unidade');
 insert into produto(id, nome, tipo) values (4, 'malha de aço', 'unidade');
 
 /*VENDEDORES*/
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (1, 'carlos@gmail.com', 'vendedor', 'carlos', '12345');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (2, 'wallace@gmail.com', 'vendedor', 'wallace', '54321');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (3, 'larissa@gmail.com', 'vendedor', 'larissa', '223344');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (4, 'davi@gmail.com', 'vendedor', 'davi', '55566777');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (5, 'marcelo@gmail.com', 'vendedor', 'marcelo', '7778899');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (1, 'carlos@gmail.com', 'vendedor', 'carlos', '12345');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (2, 'wallace@gmail.com', 'vendedor', 'wallace', '54321');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (3, 'larissa@gmail.com', 'vendedor', 'larissa', '223344');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (4, 'davi@gmail.com', 'vendedor', 'davi', '55566777');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (5, 'marcelo@gmail.com', 'vendedor', 'marcelo', '7778899');
 
 /*CLIENTES*/
 insert into cliente(id, email, nome, fk_id_vendedor) values (1, 'brasileng@gmail.com', 'mais brasil engenharia', 1);
@@ -71,6 +73,7 @@ insert into planejamento(id, fk_id_cliente, fk_id_produto, fk_id_vendedor) value
 insert into planejamento(id, fk_id_cliente, fk_id_produto, fk_id_vendedor) values (5, 2, 2, 2);
 
 
+insert into predicao(id, dia, mes, quantidade, fk_id_planejamento) values (1, '2023-05-10', '2023-05-10', 300, 2);
 
 
 
