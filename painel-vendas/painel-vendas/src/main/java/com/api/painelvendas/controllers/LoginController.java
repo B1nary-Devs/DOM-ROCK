282a79fa1cb531a30c38b16acc065017e10fa05c
GuiAlvesdev <guialves1353@gmail.com>
2023-06-02 09:25:35 -0300

    correcoes do login :bug:

--- painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/controllers/LoginController.java
+++ painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/controllers/LoginController.java
@@ -1,8 +1,11 @@
 package com.api.painelvendas.controllers;
 
 import com.api.painelvendas.dtos.VendedorLoginDto;
+import com.api.painelvendas.models.Administrador;
+import com.api.painelvendas.models.UserLogin;
 import com.api.painelvendas.models.Vendedor;
 import com.api.painelvendas.repositories.VendedorRepository;
+import com.api.painelvendas.services.AdministradorService;
 import com.api.painelvendas.services.VendedorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,10 @@ public class LoginController {
     @Autowired
     VendedorService vendedorService;
 
+
+    @Autowired
+    AdministradorService  administradorService;
+
 //    @PostMapping
 //    public Integer loginVendedor(@RequestBody @Valid VendedorLoginDto vendedorLoginDto) {
 //        Integer id = vendedorService.findByEmailAndSenha(vendedorLoginDto.getEmail(), vendedorLoginDto.getSenha());
@@ -26,15 +33,22 @@ public class LoginController {
 //    }
 
     @PostMapping("/login")
-    public ResponseEntity<Vendedor> doLogin (@RequestBody Vendedor vendedor) {
-        Vendedor authenticatedVendedor = vendedorService.login(vendedor.getEmail(), vendedor.getPassword());
-        if (authenticatedVendedor != null) {
-            return ResponseEntity.ok(authenticatedVendedor);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    public ResponseEntity<?> doLogin (@RequestBody UserLogin userLogin) {
+        String email = userLogin.getEmail();
+        String senha = userLogin.getSenha();
+
+        Vendedor vendedor = vendedorService.login(email, senha);
+        Administrador administrador = administradorService.login(email, senha);
+
+        if (vendedor == null && administrador == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-    }
+        if (administrador != null) {
+            return new ResponseEntity<Administrador>(administrador, HttpStatus.OK);
+        }
 
+        return new ResponseEntity<Vendedor>(vendedor, HttpStatus.OK);
+    }
 
 }
---
+++ painel-vendas/painel-vendas/src/main/java/com/api/painelvendas/models/UserLogin.java
@@ -0,0 +1,31 @@
+package com.api.painelvendas.models;
+
+
+
+public class UserLogin {
+
+
+    private String email;
+    private String senha;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+
+}
