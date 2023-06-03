2859da39f825574dbeca1bc03596fa8adc694939
Larissa Souza <larissatomedesouza@gmail.com>
2023-06-02 22:52:03 -0300

    admin update

--- Banco de Dados/dom_rock_BD.sql
+++ Banco de Dados/dom_rock_BD.sql
@@ -66,7 +66,7 @@ fk_id_registro INT,
 FOREIGN KEY(fk_id_registro) REFERENCES registro(id));
 
 /*ADMINISTRADOR*/
-insert into administrador(id, email, senha, nivelAcesso) values (1, 'carlosadm@gmail.com', '12345', 'administrador');
+insert into administrador(id, email, password, nivelAcesso) values (1, 'carlosadm@gmail.com', '12345', 'administrador');
 
 /*PRODUTOS*/
 insert into produto(id, nome, tipo) values (1, 'chapa de aço', 'unidade');
@@ -75,11 +75,11 @@ insert into produto(id, nome, tipo) values (3, 'barra de aço', 'unidade');
 insert into produto(id, nome, tipo) values (4, 'malha de aço', 'unidade');
 
 /*VENDEDORES*/
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (1, 'carlos@gmail.com', 'vendedor', 'carlos', '12345');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (2, 'wallace@gmail.com', 'vendedor', 'wallace', '54321');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (3, 'larissa@gmail.com', 'vendedor', 'larissa', '223344');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (4, 'davi@gmail.com', 'vendedor', 'davi', '55566777');
-insert into vendedor(id, email, nivel_acesso, nome, senha) values (5, 'marcelo@gmail.com', 'vendedor', 'marcelo', '7778899');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (1, 'carlos@gmail.com', 'vendedor', 'carlos', '12345');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (2, 'wallace@gmail.com', 'vendedor', 'wallace', '54321');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (3, 'larissa@gmail.com', 'vendedor', 'larissa', '223344');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (4, 'davi@gmail.com', 'vendedor', 'davi', '55566777');
+insert into vendedor(id, email, nivel_acesso, nome, password) values (5, 'marcelo@gmail.com', 'vendedor', 'marcelo', '7778899');
 
 /*CLIENTES*/
 insert into cliente(id, email, nome, fk_id_vendedor) values (1, 'brasileng@gmail.com', 'mais brasil engenharia', 1);
---
+++ view/javascript/script_clientes_adm.js
@@ -0,0 +1,30 @@
+async function CarregarCarteiraClientesAdmin() {
+    try {
+        //redireciona a tela com o vendedor logado
+     
+        const response = await axios.get(`http://localhost:8080/cliente`);
+        const clientes = response.data;
+    
+  
+      const selectClientes = document.getElementById('tabelaClientes');
+  
+      clientes.forEach(cliente => {
+        const linha = document.createElement('tr');
+        const idCliente = document.createElement ('td')
+        idCliente.textContent = cliente.id
+        const nomeVendedor = document.createElement('td')
+        nomeVendedor.textContent = cliente.vendedor.nome
+        const nomeCliente = document.createElement ('td')
+        nomeCliente.textContent = cliente.nome
+        const emailCliente = document.createElement ('td')
+        emailCliente.textContent = cliente.email
+        linha.appendChild(idCliente)
+        linha.appendChild(nomeVendedor)
+        linha.appendChild(nomeCliente)
+        linha.appendChild(emailCliente)
+        selectClientes.appendChild(linha)
+      });
+    } catch (error) {
+      console.error(error)
+    }
+  }
